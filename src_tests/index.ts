5e9f8388c0b531d83a216a9d232572d86b9b0062
Anton Barinov <anton@barinov.com>
2023-02-12 15:59:34 +0400

    initial

---
+++ src_tests/index.ts
@@ -0,0 +1,33 @@
+import { autorun, reactive } from '../src';
+
+
+class Test {
+    set = new Set();
+    map = new Map();
+
+    constructor() {
+        reactive(this);
+    }
+}
+
+const test = new Test();
+
+const obj1 = { a: 1 };
+const obj2 = { a: 2 };
+
+autorun(() => {
+    console.log('set has obj1', test.set.has(obj1));
+});
+
+autorun(() => {
+    console.log('set size', test.set.size);
+});
+
+test.set.add(obj2);
+test.set.add(obj2);
+test.set.add(obj1);
+test.set.delete(obj2);
+test.set.delete(obj1);
+
+test.set.add(obj1);
+test.set.delete(obj1);
