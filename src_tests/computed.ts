dcc172a2485a14580b10a679dac24fc8860a34cc
Anton Barinov <anton@barinoff.com>
2024-04-14 14:19:15 +0400

    initial

--- src_tests/computed.ts
+++ src_tests/computed.ts
@@ -15,6 +15,11 @@ class Test {
         console.log('exeс mult getter');
         return this.counter * 2;
     }
+
+    get mult2() {
+        console.log('exeс mult2 getter');
+        return this.mult * 2;
+    }
 }
 
 const test = new Test(1);
@@ -23,11 +28,26 @@ const test2 = new Test(2);
 console.log(test.mult);
 console.log(test2.mult);
 
+console.log(test.mult2);
+console.log(test2.mult2);
+
 console.log(test.mult);
 console.log(test2.mult);
+
+console.log(test.mult2);
+console.log(test2.mult2);
 
 test.counter = 3;
 test2.counter = 4;
 
 console.log(test.mult);
 console.log(test2.mult);
+
+console.log(test.mult2);
+console.log(test2.mult2);
+
+console.log(test.mult);
+console.log(test2.mult);
+
+console.log(test.mult2);
+console.log(test2.mult2);
--- src_tests/index.ts
+++ src_tests/index.ts
@@ -5,6 +5,6 @@
 //import './other';
 //import './other2';
 //import './reactions';
-import './arrays';
-//import './computed';
+//import './arrays';
+import './computed';
 
