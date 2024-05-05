e85aeb94fd7e57acaaf26022bd55b1653f4238c3
Anton Barinov <anton@barinoff.com>
2024-05-05 19:41:01 +0400

    initial

--- src_tests/helpers.ts
+++
@@ -1,5 +0,0 @@
-export function sleep(ms = 5) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
-}
--- src_tests/index.ts
+++
@@ -1,6 +0,0 @@
-//import './other';
-//import './other2';
-//import './reactions';
-//import './arrays';
-//import './computed';
-
